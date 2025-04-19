212bef85fe7c5164a76057a04731492b327a3941
bruderj15 <julian.bruder@outlook.com>
2025-04-19 21:02:43 +0200

    main: add test-api-macros

---
+++ test-api-macros/src/lib.rs
@@ -0,0 +1,28 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, ItemFn};
+
+#[proc_macro_attribute]
+pub fn blitzfilter_dynamodb_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(item as ItemFn);
+    let fn_name = &input.sig.ident;
+    let fn_block = &input.block;
+
+    let result = quote! {
+        #[serial_test::serial]
+        #[tokio::test]
+        async fn #fn_name() {
+            let container = test_api::dynamodb::get_localstack_dynamodb().await; 
+            let client = test_api::dynamodb::get_client().await;
+
+            test_api::dynamodb::setup(client).await;
+
+            let test_fn = async #fn_block;
+            test_fn.await;
+
+            test_api::dynamodb::reset(client).await;
+        }
+    };
+
+    result.into()
+}
