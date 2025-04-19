8034a66fa7d0a5f54454e0b3b167d4601efda7f9
bruderj15 <julian.bruder@outlook.com>
2025-04-19 22:17:51 +0200

    main: drop existing container on start

--- src/localstack.rs
+++ src/localstack.rs
@@ -1,16 +1,20 @@
 use std::collections::HashMap;
+use std::process::Command;
 use testcontainers::core::IntoContainerPort;
 use testcontainers::runners::AsyncRunner;
 use testcontainers::{ContainerAsync, ImageExt};
 use testcontainers_modules::localstack::LocalStack;
 
+const LOCALSTACK_CONTAINER_NAME: &str = "localstack-test-api";
+
 pub async fn spin_up_localstack(env_vars: HashMap<&str, &str>) -> ContainerAsync<LocalStack> {
+    cleanup_existing_container(LOCALSTACK_CONTAINER_NAME);
     let request = env_vars
         .iter()
         .fold(
             LocalStack::default()
                 .with_tag("latest")
-                .with_container_name("localstack-test-api"),
+                .with_container_name(LOCALSTACK_CONTAINER_NAME),
             |ls, (k, v)| ls.with_env_var(*k, *v),
         )
         .with_mapped_port(4566, 4566.tcp());
@@ -27,4 +31,8 @@ pub async fn spin_up_localstack(env_vars: HashMap<&str, &str>) -> ContainerAsync
 
 pub async fn spin_up_localstack_with_services(services: &[&str]) -> ContainerAsync<LocalStack> {
     spin_up_localstack(HashMap::from([("SERVICES", services.join(",").as_str())])).await
+}
+
+fn cleanup_existing_container(name: &str) {
+    let _ = Command::new("docker").args(["rm", "-f", name]).output();
 }
