cbb7c040a1b6bb8bdaff5f8b77422fcbdf6c85ba
bruderj15 <julian.bruder@outlook.com>
2025-04-25 19:23:33 +0200

    main: add `Generator::generate_n` with compile-time length

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -9,4 +9,11 @@ pub trait Generator {
     {
         (0..number).map(|_| Self::generate()).collect()
     }
+
+    fn generate_n<const N: usize>() -> [Self; N]
+    where
+        Self: Sized,
+    {
+        std::array::from_fn(|_| Self::generate())
+    }
 }
